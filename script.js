7e74b2e9856e9d407044c460e4bad3db3d723d4e
DanilloAzevedo <danillo.azevedo2015@gmail.com>
2026-07-14 20:15:16 -0300

    Mudança da inserção de button list

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
-const string = `[
-    {
-        "link":"https://ferronaveia.com.br/treinos-de-academia-completo-todos-os-niveis"
-    },
-    {
-        "link":"https://www.tuasaude.com/dieta-para-emagrecer/"
-    },
-    {
-        "link":"https://www.tuasaude.com/melhor-exercicio-para-emagrecer/"
-    }
-]`;
+const string = [
+   {
+      "name":"Treinos de Musculação",
+      "link":"https://ferronaveia.com.br/treinos-de-academia-completo-todos-os-niveis"
+   },
+   {
+      "name":"Tipos de Dietas",
+      "link":"https://www.tuasaude.com/dieta-para-emagrecer/"
+   },
+   {
+      "name":"Aeróbicos",
+      "link":"https://www.tuasaude.com/melhor-exercicio-para-emagrecer/"
+   }
+];
 
-const json = JSON.parse(string);
+//const json = JSON.parse(string);
+container_links = document.getElementById("container-links");
+
+string.forEach((link, indice) => {
+
+    var elem = "<div id='bt"+indice+"' class='buttonlist'>"+ link.name + "</div>";
+    container_links.innerHTML =   container_links.innerHTML + elem;
+
+     
+});
+
 
 const botoes = document.querySelectorAll(".buttonlist");
-
 botoes.forEach((botao, indice) => {
     botao.style.cursor = "pointer";
 
     botao.addEventListener("click", function () {
-        window.location.href = json[indice].link;
+        window.location.href = string[indice].link;
     });
 });
